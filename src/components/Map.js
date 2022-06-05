a825d398d1e265efc3531f7673e85b9ae9539dab
SarkerPabon <sarkerpabon@gmail.com>
2022-06-05 14:04:03 +0600

    Add Map

---
+++ src/components/Map.js
@@ -0,0 +1,38 @@
+import L from "leaflet";
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "./Map.css";
+
+let DefaultIcon = L.icon({
+	iconUrl: icon,
+	shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const Map = () => {
+	return (
+		<section className='card my-5  bg-base-100 shadow-2xl container mx-auto p-6'>
+			<h1 className='text-center my-3 text-4xl font-bold text-gray-500'>
+				Our Location
+			</h1>
+			<MapContainer
+				center={[23.7934, 90.4064]}
+				zoom={16}
+				scrollWheelZoom={false}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+				/>
+				<Marker position={[23.7934, 90.4064]}>
+					<Popup>Megento</Popup>
+				</Marker>
+			</MapContainer>
+		</section>
+	);
+};
+
+export default Map;
